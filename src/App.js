fc8af9f12f1e428d6ee0cf2636c042943861ced2
br305893 <ben.rosner.williamsburg@gmail.com>
2019-09-08 14:54:14 -0400

    finish styling of home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import {Link, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div className="nav-container">
+          <nav className="navigation">
+            <Link to="/work">Work</Link>
+            <Route exact path="/work"></Route>
+            <Link to="/about">About</Link>
+            <Route exact path="/about"></Route>
+          </nav>
+        </div>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+      <footer className="app-footer">   
+        <div className = "quote-container">
+          <p className="quote">"Army of one now, don’t let down your guard" - Page McConnell, Phish</p>
+          </div>
+      </footer>
+    )
+  }
+
+export default Footer
