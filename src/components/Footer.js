f3af5e9ee51a1aaa3dac896303f7bf0e7fc28255
br305893 <ben.rosner.williamsburg@gmail.com>
2019-10-17 12:19:17 -0400

    add resume logo and link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Footer.css'
 import LinkedInLogo from "../assets/LinkedIn_logo_initials.png"
 import GitHubLogo from "../assets/GitHub-Mark.png"
+import ResumeLogo from '../assets/Resume-Logo.png'
 
 const Footer = () => {
     return (
@@ -12,6 +13,7 @@ const Footer = () => {
          <div className="social-media-links">
         <a href="https://www.linkedin.com/in/benrosner/"><img class="linkedin-logo" src={LinkedInLogo} alt="LinkedIn Logo"/></a>
         <a href="https://github.com/benrosnerwilliamsburg/"><img class="github-logo" src={GitHubLogo} alt="GitHub Logo"/></a>
+        <a href="https://drive.google.com/file/d/1lpltEDNhNYTOHZZIz0LxNr5TJhMLMsm_/view?usp=sharing"><img class="resume-logo" src={ResumeLogo} alt="resume"/></a>
         </div>
       </footer>
     )
