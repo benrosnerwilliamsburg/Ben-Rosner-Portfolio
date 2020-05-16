b6892c21fee5ce763ed1a0dc6404d0522743e6ae
br305893 <ben.rosner.williamsburg@gmail.com>
2020-05-16 14:53:03 -0400

    change position of work icons and delete summaries

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,30 +9,17 @@ const Work = () => {
     return (
         <div className = "work-container">
             <div className="image-container">
-              <a href="https://asailorslifeforme.org/"><img src={Sailors} alt="Sailors Website" className="project"/></a>
-            </div>
-            <div className="summary-container">
-              <h4 className="summary">Developed an educational Wordpress website for the USS Constitution Museum in Boston, Massachusetts.</h4>
+              <a href="https://asailorslifeforme.org/"><img src={Sailors} alt="Sailors Website" className="project" width="713px" height="348px" /></a>
             </div>
             <div className="image-container">
               <a href="https://github.com/benrosnerwilliamsburg/go-go-giphy"><img src={GoGoGiphy} alt="Go Go Giphy App" className="project"/></a>
             </div>
-            <div className="summary-container">
-              <h4 className="summary">Created a GIF search app using the Giphy.com RESTful API and Javascript with React.</h4>
-            </div>
             <div className="image-container">
                 <a href="https://github.com/c-kehler/Better-Living"><img src={BetterLiving} alt="Better Living App" className="project"/></a>
-            </div>
-            <div className="summary-container">
-              <h4 className="summary"> Developed a front-end web application to monitor health and wellness using UX concepts.</h4>
             </div>
             <div className="image-container">
               <a href="https://github.com/benrosnerwilliamsburg/vivyl-app"><img src={Revivyl} alt="Revivyl App" className="project"/></a>  
             </div>
-            <div className="summary-container">
-              <h4 className="summary">Created a vinyl record search app using the Discogs.com RESTful API and Javascript with React.</h4>
-            </div>
-            
         </div>
     )
   }
