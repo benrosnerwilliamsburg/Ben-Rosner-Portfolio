5efcd9fe822fda3ac9917a0c89c630cd4cb63e2c
br305893 <ben.rosner.williamsburg@gmail.com>
2020-11-18 15:06:02 -0500

    clean up and lint code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,9 @@ const Footer = () => {
       <footer className="app-footer">   
         <p className="quote">"Dripping in this strange design/None is yours and far less mine" - Anastasio/Marshall, Phish</p>
          <div className="social-media-links">
-        <a href="https://www.linkedin.com/in/benrosner/"><img class="linkedin-logo" src={LinkedInLogo} alt="LinkedIn Logo"/></a>
-        <a href="https://github.com/benrosnerwilliamsburg/"><img class="github-logo" src={GitHubLogo} alt="GitHub Logo"/></a>
-        <a href="https://drive.google.com/file/d/1GgLcA4QERdfexd6dsjVwh15lthOKRe4C/view?usp=sharing"><img class="resume-logo" src={ResumeLogo} alt="resume"/></a>
+        <a href="https://www.linkedin.com/in/benrosner/"><img className="linkedin-logo" src={LinkedInLogo} alt="LinkedIn Logo"/></a>
+        <a href="https://github.com/benrosnerwilliamsburg/"><img className="github-logo" src={GitHubLogo} alt="GitHub Logo"/></a>
+        <a href="https://drive.google.com/file/d/1GgLcA4QERdfexd6dsjVwh15lthOKRe4C/view?usp=sharing"><img className="resume-logo" src={ResumeLogo} alt="resume"/></a>
         </div>
       </footer>
     )
