46e538756a1e640349ef304f8320b5a1143da2a3
br305893 <ben.rosner.williamsburg@gmail.com>
2020-11-18 15:01:38 -0500

    update work page and change color scheme

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import './Work.scss';
 import GoGoGiphy from '../assets/go_go_giphy.png'
-import Revivyl from '../assets/revivyl.png'
-import BetterLiving from '../assets/better_living.png'
 import Sailors from '../assets/sailors.png'
 
 const Work = () => {
     return (
-        <div className = "work-container">
+        <div className="work-container">
             <div className="image-container">
               <a href="https://asailorslifeforme.org/"><img src={Sailors} alt="Sailors Website" className="project" width="713px" height="348px" /></a>
             </div>
+          <div className='desc'>
+            <p>Developed educational Wordpress website for noteworthy museum in Boston, Massachusetts.</p>
+          </div>
             <div className="image-container">
               <a href="https://go-go-giphy.surge.sh/"><img src={GoGoGiphy} alt="Go Go Giphy App" className="project"/></a>
             </div>
-            <div className="image-container">
-                <a href="https://github.com/c-kehler/Better-Living"><img src={BetterLiving} alt="Better Living App" className="project"/></a>
-            </div>
-            <div className="image-container">
-              <a href="https://github.com/benrosnerwilliamsburg/vivyl-app"><img src={Revivyl} alt="Revivyl App" className="project"/></a>  
-            </div>
+            <div className='desc'>
+              <p>Created a GIF search app using the Giphy.com RESTful API and JavaScript with React.</p>
+          </div> 
         </div>
     )
   }
